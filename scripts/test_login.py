de7be4de55d9911395ed06ba821d2e358d9980cc
Jeryydw <ddingc@163.com>
2018-09-23 11:09:58 +0800

    新建项目

---
+++ scripts/test_login.py
@@ -0,0 +1,12 @@
+class TestLogin:
+    def test_login1(self):
+        print('对')
+        assert 1
+
+    def test_login2(self):
+        print('错')
+        assert 1
+
+    def test_login3(self):
+        print('对')
+        assert 1
