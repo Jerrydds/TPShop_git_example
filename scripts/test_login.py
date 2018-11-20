9c2ec6dd69b46965894eaa379f5202c718c9e057
XiaoMing <666@163.com>
2018-11-20 12:59:36 +0800

    添加参数化

--- scripts/test_login.py
+++ scripts/test_login.py
@@ -11,22 +11,22 @@ class TestLogin:
         self.driver = init_driver()
         self.page = Page(self.driver)
 
-    # @pytest.mark.parametrize("args", analyze_with_file_name("login", "test_login"))
-    # def test_login(self, args):
-    def test_login(self):
-        # phone = args["phone"]
-        # password = args["password"]
-        # expect = args["expect"]
+    @pytest.mark.parametrize("args", analyze_with_file_name("login", "test_login"))
+    def test_login(self, args):
+    # def test_login(self):
+        phone = args["phone"]
+        password = args["password"]
+        expect = args["expect"]
 
         # 主页点击我的
         self.page.home.click_mine()
         # 我的点击登录
         self.page.mine.click_sign_up_and_login()
         # 输入用户名
-        self.page.sign_up_and_login.input_phone('18503080305')
+        self.page.sign_up_and_login.input_phone(phone)
         # 输入密码
-        self.page.sign_up_and_login.input_password('123456')
+        self.page.sign_up_and_login.input_password(password)
         # 点击登录
         self.page.sign_up_and_login.click_login()
         # 判断toast是否存在
-        assert self.page.sign_up_and_login.is_toast_exits('登录成功')
+        assert self.page.sign_up_and_login.is_toast_exits(expect)
